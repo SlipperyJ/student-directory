510107f0ac5d169c46bf53fb3000aa91b0254218
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-06-26 21:31:23 +0100

    format text output

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts " Please enter the names of the students ".center(95, '*-')
+  puts " To finish, just hit return twice ".center(95, '-*')
   name = gets.chomp
   questions(name)
 end
@@ -9,18 +9,17 @@ def questions(name)
   students = []
 
   while !name.empty? do
-    puts "Please enter a hobby"
+    puts "Please enter a hobby".center(95)
     hobby = gets.chomp
-    puts "Please enter country of birth"
+    puts "Please enter country of birth".center(95)
     birth_country = gets.chomp
-    puts "Please enter height"
+    puts "Please enter height".center(95)
     height = gets.chomp
     students << {name: name, cohort: :november, hobby: hobby, birth_country:
                  birth_country, height: height}
     puts "Now we have #{students.count} students"
-
-    puts "Please enter another name"
-    puts "To finish, just hit return twice"
+    puts "Please enter another name".center(95, '-*')
+    puts "To finish, just hit return twice".center(95, '-*')
     name = gets.chomp
   end
   students
@@ -40,12 +39,12 @@ def name_restrictions(students)
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(95)
+  puts "-------------".center(95)
 end
 
 def print(students)
-  puts "enter a letter to filter by intial, or leave blank for no filter"
+  puts "enter a letter to filter by intial, or leave blank for no filter".center(95)
   @letter = gets.chomp!
   students = letter_filter(students, @letter)
 
@@ -57,7 +56,7 @@ def print(students)
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(95)
 end
 
 students = input_students
